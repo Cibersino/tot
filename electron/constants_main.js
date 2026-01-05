6b7f4aea8e7853d24cef41d6c0a90b3e8ec19f93
Cibersino <manuel.neira@gmail.com>
2026-01-05 03:45:14 -0300

    too large guard

--- electron/constants_main.js
+++ electron/constants_main.js
@@ -2,7 +2,11 @@
 'use strict';
 
 const MAX_TEXT_CHARS = 10000000;
+const MAX_IPC_MULTIPLIER = 4;
+const MAX_IPC_CHARS = MAX_TEXT_CHARS * MAX_IPC_MULTIPLIER;
 
 module.exports = {
   MAX_TEXT_CHARS,
+  MAX_IPC_MULTIPLIER,
+  MAX_IPC_CHARS,
 };
