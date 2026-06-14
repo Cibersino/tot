9e7add24d7fc7f9c442e2bfa8ce8d4d1dae708f2
Cibersino <manuel.neira@gmail.com>
2026-06-14 00:04:01 -0400

    layout tweak

--- electron/main.js
+++ electron/main.js
@@ -499,7 +499,7 @@ function createMainWindow() {
   // NOTE: useContentSize:true => width/height apply to the content area (exclude window borders).
   mainWin = new BrowserWindow({
     width: 828,
-    height: 444,
+    height: 450,
     useContentSize: true,
     resizable: false,   // Fixed size for consistent layout
     maximizable: false, // Prevent maximize button from changing size
